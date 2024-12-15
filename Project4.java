eba44c7da682cda0ec0a642f2741b3ce3d29168c
Anish Patel <patelanish203@gmail.com>
2024-12-15 11:35:37 -0500

    Update Project4.java

--- Project4.java
+++ Project4.java
@@ -1,15 +1,3 @@
-/**
- * Anish Patel
- * COP3503
- * Project Objects & Classes
- * 12/1/24
- * 
- * This program creates a custom GUI that allows the users to enter in dietary 
- * information into a survey and then clear or submit the answer fields. The submit
- * button will record the answers into s .csv file and clear all the text fields
- * to prepare for the next user. The clear button allows you to restart the form
- * without submitting your into into the survey results file.
- */
 public class Project4 {
 
 	public static void main(String[] args)
